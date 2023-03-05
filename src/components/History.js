7b7a6a00e2883540c2acbf430235d310b5d1b7da
aquila39 <86932476+aquila39@users.noreply.github.com>
2023-03-05 15:44:52 +0100

    Update history view

--- src/components/History.js
+++ src/components/History.js
@@ -1,4 +1,5 @@
 import useFetch from './utility/Fetch';
+import { Link } from "react-router-dom";
 
 function History() {
     const url = 'http://principessedisney.duckdns.org:8000/api/games';
@@ -7,11 +8,18 @@ function History() {
 
     return (
         <div>
-            <p>Ciao1 </p>
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
-            {data}
-            <p>Ciao</p>
+            {data && data.map(match => (
+
+                <Link to={'/history/' + match.id} key={match.id} className={'d-flex m-3 border'}>
+                    <p>{match.nameA}: </p>
+                    <p>{match.pointA} vs </p>
+                    <p>{match.nameB}: </p>
+                    <p>{match.pointB}</p>
+                </Link>
+
+            ))}
         </div>
     );
 }
