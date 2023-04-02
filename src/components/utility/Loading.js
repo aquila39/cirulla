0b0624abdd2f36271aed9eefca62a94d49f911b6
aquila39 <86932476+aquila39@users.noreply.github.com>
2023-04-02 17:52:43 +0200

    Fix spinner

--- src/components/utility/Loading.js
+++ src/components/utility/Loading.js
@@ -1,8 +1,8 @@
 function Loading() {
 
     return (
-        <div class="d-flex justify-content-center">
-            <div class="spinner-border m-5" role="status">
+        <div className="d-flex justify-content-center">
+            <div className="spinner-border m-5" role="status">
                 {/* <span class="sr-only">Loading...</span> */}
             </div>
         </div>
