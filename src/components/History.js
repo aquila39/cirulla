ba6716bfaaee94ac9c2dce75f4fa752f1a96c607
aquila39 <86932476+aquila39@users.noreply.github.com>
2023-04-02 18:11:50 +0200

    Update history view

--- src/components/History.js
+++ src/components/History.js
@@ -10,6 +10,9 @@ function History() {
 
     return (
         <div id='mainHistory'>
+
+            <h1 className='m-3 text-center'>Storico partite</h1>
+
             {error && <div>{error}</div>}
             {isPending && <Loading />}
             {data && data.map(match => (
@@ -25,17 +28,17 @@ function History() {
                         status: match.status
                     }}
                     key={match.id}
-                    className={`d-flex border text-center m-3 py-2 px-4 justify-content-center align-items-center fs-4 text-decoration-none ${match.status === 'end' ? 'bg-success' : 'bg-warning blink-long'}`
+                    className={`d-flex border text-center m-3 py-2 px-4 align-items-center fs-4 text-decoration-none ${match.status === 'end' ? 'bg-success' : 'bg-warning blink-long'}`
                     }>
 
-                    <div className='me-auto d-lg-inline-flex'>
+                    <div className='me-auto d-lg-inline-flex' style={{ width: '40%' }}>
                         <p className='text-break'>{match.nameA}</p>
                         <p className='ms-lg-5'>{match.pointA}</p>
                     </div>
 
-                    <img src={vs_logo} alt='vs icon' className='mx-5' height='50rem' />
+                    <img src={vs_logo} alt='vs icon' className='mx-1 img-fluid' style={{ width: '3rem' }} />
 
-                    <div className='ms-auto d-lg-inline-flex flex-row-reverse'>
+                    <div className='ms-auto d-lg-inline-flex flex-row-reverse' style={{ width: '40%' }}>
                         <p className='text-break'>{match.nameB}</p>
                         <p className='me-lg-5'>{match.pointB}</p>
                     </div>
